bd7f6ac150a751086f9734929b89db3563a5ff0b
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-06-13 11:18:57 +0700

    init prisma module

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const users = ref([])
+
+onMounted(async () => {
+  users.value = await fetch('/api/users').then((res) => res.json())
+})
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }}
+        <br>
+        {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
